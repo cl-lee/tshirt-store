0d8a66ff4ac962a30e0bbbf625fca2dae66b3d8a
cl.lee <cl.lee.ofz@gmail.com>
2023-06-19 19:17:57 +0100

    added shopping cart icon

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,18 +1,39 @@
 import logo from "../../Images/store-logo.jpeg";
-import './NavBar.css';
+import "./NavBar.css";
+
+// for shopping cart icon from fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
   return (
     <nav className="h-20 fixed top-0 left-0 right-0 flex pl-4">
-
       {/* Shop logo */}
-      <img className="w-20 h-20" src={logo} alt="T-shirt Emporium logo"/>
+      <img className="w-20 h-20" src={logo} alt="T-shirt Emporium logo" />
 
       {/* Shop name (responsive, hidden in mobile screens) */}
-      <h1>Shop Name</h1>
 
+      {/* flex grow? (Tailwind: grow)*/}
+      {/* DONE horizontal align middle (CSS: justify-content: center)?? */}
+      {/* DONE vertical align middle (CSS: text-align: center; tailwind: text-center)*/}
+      {/* DONE change font size */}
+      {/* DONE change font - serif*/}
+      <h1 className="grow self-center text-center text-4xl font-serif font-medium">
+        Shop Name
+      </h1>
 
+      {/* shop basket icon */}
 
+      {/* from font awesome */}
+      {/* DONE 1. install SVG core npm i --save @fortawesome/fontawesome-svg-core */}
+      {/* DONE 2. Add Icon Packages 
+      # Free icons styles
+      npm i --save @fortawesome/free-solid-svg-icons
+      npm i --save @fortawesome/free-regular-svg-icons
+      */}
+      {/* DONE 3. Add the React Component 
+      npm i --save @fortawesome/react-fontawesome@latest */}
+      <FontAwesomeIcon icon={faCartShopping} />
     </nav>
   );
 }
